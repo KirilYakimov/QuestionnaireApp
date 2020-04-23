cfba9dd256945abdf51e621df0c2c86fde3b3c08
Kiril Yakimov <kiril.yakimov@gmail.com>
2020-04-23 16:56:19 +0300

    Initial commit

---
+++ QuestionnaireWebsite/Controllers/InfoController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace QuestionnaireWebsite.Controllers
+{
+    public class InfoController : Controller
+    {
+        [Route("Info/About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        [Route("Info/Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
---
+++ QuestionnaireWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace QuestionnaireWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
